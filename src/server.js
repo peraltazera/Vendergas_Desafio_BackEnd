c7728316f25fc6300780152fe99a1617068f7981
peraltazera <victorgamedeveloper@gmail.com>
2024-06-10 16:08:02 -0300

    Implementação do sistema de rotas
    Desenvolve e integra um sistema de roteamento para direcionar as requisições HTTP.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const loaders = require("./apps/configs/loaders");
+const routes = require("./apps/routes/routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(routes)
 loaders.start();
 
 const PORT = process.env.PORT || 3000;
